a08109f4d1e1df71ca99cad7bb23612d98803743
Vanessa Viana <vanessaviana30@gmail.com>
2023-02-27 19:08:36 -0300

    Adding 2 test cases

---
+++ Object Repository/LOGIN/mensagem_email_invalido.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>mensagem_email_invalido</name>
+   <tag></tag>
+   <elementGuidId>75621ddc-8026-4ba8-aff9-668e092ca366</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>p.alert-content</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//main[@id='main-content']/section/div/div/form/div/div/p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>5e73a26c-09c6-4ab8-b352-35288dfdd58d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>alert-content</value>
+      <webElementGuid>6d954cce-58c4-4a62-bb63-eff3f938cb68</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Insira um e-mail ou número de celular válido.</value>
+      <webElementGuid>bb54c25d-41c5-4e83-98d2-4b685b8d7d06</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main-content&quot;)/section[@class=&quot;section min-h-[560px] flex-nowrap pt-[40px] babybear:flex-col babybear:min-h-[0] babybear:px-mobile-container-padding babybear:pt-[24px]&quot;]/div[@class=&quot;self-start relative flex-shrink-0 w-[55%] pr-[42px] babybear:w-full babybear:pr-[0px]&quot;]/div[@class=&quot;sign-in-form-container&quot;]/form[@class=&quot;sign-in-form hero-cta-form&quot;]/div[@class=&quot;alert&quot;]/div[@class=&quot;wrapper&quot;]/p[@class=&quot;alert-content&quot;]</value>
+      <webElementGuid>bfe16520-c711-4ee1-8169-86b657090bee</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//main[@id='main-content']/section/div/div/form/div/div/p</value>
+      <webElementGuid>27db311d-cbe9-4c5c-b745-ffaed3c488e4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Conheça sua comunidade profissional'])[1]/following::p[1]</value>
+      <webElementGuid>0ad81688-7c18-4143-91dd-5a83ffa9cddd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Entrar'])[1]/following::p[1]</value>
+      <webElementGuid>282f9165-8413-40ca-a0d0-5c53ce833909</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='E-mail ou número de telefone'])[1]/preceding::p[1]</value>
+      <webElementGuid>5ba4e458-f92f-418c-92b9-2df24f34c8bd</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Senha'])[1]/preceding::p[1]</value>
+      <webElementGuid>515ea85a-ad6c-45de-9f14-38699873a5bf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Insira um e-mail ou número de celular válido.']/parent::*</value>
+      <webElementGuid>d072d3ac-9ba1-4b86-9a65-36922bfdf199</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//p</value>
+      <webElementGuid>1b24e449-073d-413c-ad27-cdc4d3d6cbbe</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = 'Insira um e-mail ou número de celular válido.' or . = 'Insira um e-mail ou número de celular válido.')]</value>
+      <webElementGuid>ec70731f-2bfd-45f3-b93f-ee00238a8922</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/LOGIN.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LOGIN</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>060b3f76-1682-4bc1-b0e0-cfeab387042a</testSuiteGuid>
+   <testCaseLink>
+      <guid>d84174d1-6ff9-4820-9235-ddf84b3dd329</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Caso 1 - Usuário tenta informar uma senha inválida</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3e590a6-6d23-4c76-a44f-e00a44ec1027</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Caso 2 - Usuário tenta informar um e-mail inválido</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
