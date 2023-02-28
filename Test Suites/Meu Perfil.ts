a9358dd195f2b2fc2c2fc4e12335cc2666e5528c
Vanessa Viana <vanessaviana30@gmail.com>
2023-02-27 21:16:03 -0300

    Adding Object Repository and Test Suites

---
+++ Test Suites/Meu Perfil.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Meu Perfil</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4b91f7a0-9ed2-42bf-90c6-809d40df814c</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Minha Rede.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Minha Rede</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a304bc9e-4fe3-438d-96a1-e08fbf9b5c72</testSuiteGuid>
+</TestSuiteEntity>
