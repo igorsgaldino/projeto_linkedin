023071f03d76e09b3e8a41f8b07ea6fde4386d83
igorsgaldino <igorsgaldino96@gmail.com>
2023-03-02 21:38:21 -0300

    teste

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -15,28 +15,28 @@
       <guid>d84174d1-6ff9-4820-9235-ddf84b3dd329</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Caso 1 - Usuário tenta informar uma senha inválida</testCaseId>
+      <testCaseId>Test Cases/Login/Caso 1 - Usuário tenta informar uma senha inválida</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>f3e590a6-6d23-4c76-a44f-e00a44ec1027</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Caso 2 - Usuário tenta informar um e-mail inválido</testCaseId>
+      <testCaseId>Test Cases/Login/Caso 2 - Usuário tenta informar um e-mail inválido</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>86508125-c1fd-4e2d-84b1-4ece5a22b08c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Caso 3 - Usuário deixa de informar uma senha</testCaseId>
+      <testCaseId>Test Cases/Login/Caso 3 - Usuário deixa de informar uma senha</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>83307e20-1f9c-4632-b6a7-ece98e957f50</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Caso 4 - Usuário deixa de informar um e-mail</testCaseId>
+      <testCaseId>Test Cases/Login/Caso 4 - Usuário deixa de informar um e-mail</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
